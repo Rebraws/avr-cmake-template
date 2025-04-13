29010ea1952672f1dcf96cbf5b2dff7357d412a5
Rebraws <nahuel.braian@gmail.com>
2025-04-13 19:57:37 -0300

    build: Add CMake configuration for AVR build and flash

--- cmake/avr-toolchain.cmake
+++ cmake/avr-toolchain.cmake
@@ -1,2 +1,33 @@
+
+find_program(AVR_CC avr-gcc-14 REQUIRED)
+find_program(AVR_CXX avr-g++-14 REQUIRED)
+find_program(AVR_OBJCOPY avr-gcc-14 REQUIRED)
+find_program(AVR_SIZE avr-size REQUIRED)
+find_program(AVR_OBJDUMP avr-objdump REQUIRED)
+
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR avr)
+set(CMAKE_CXX_COMPILER ${AVR_CXX})
+set(CMAKE_C_COMPILER ${AVR_CC})
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+# Microcontroller and clock frequeny
+set(AVR_MCU "atmega328p")
+message(STATUS "AVR Toolchain: Target MCU set to ${AVR_MCU}")
+set(F_CPU "16000000UL")
+message(STATUS "AVR Toolchain: Target F_CPU set to ${F_CPU}")
+
+# Flags
+set(CMAKE_CXX_FLAGS "-mmcu=${AVR_MCU} -DF_CPU=${F_CPU} -Os -Wall -Wextra -flto -fno-exceptions")
+
+set(CMAKE_EXE_LINKER_FLAGS "-mmcu=${AVR_MCU} -Wl,--gc-sections -flto")
+
+set(CMAKE_C_COMPILER_WORKS 1)
+set(CMAKE_CXX_COMPILER_WORKS 1)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
--- cmake/flash.cmake
+++ cmake/flash.cmake
@@ -1,5 +1,26 @@
 # script to generate binary and hex format for flashing
 
+find_program(AVRDUDE avrdude REQUIRED)
 
-if(AVRDUDE)
 
+set(AVRDUDE_PROGRAMMER arduino)
+set(AVRDUDE_PORT /dev/ttyUSB0)
+set(AVRDUDE_BAUDRATE "115200")
+
+set(AVRDUDE_MCU m328p CACHE STRING "")
+
+add_custom_target(flash
+  COMMAND ${AVRDUDE} 
+  -c ${AVRDUDE_PROGRAMMER}
+  -p ${AVRDUDE_MCU}
+  -P ${AVRDUDE_PORT}
+  -b ${AVRDUDE_BAUDRATE}
+  -D
+  -U flash:w:${PROJECT_HEX_FILE}:i
+  DEPENDS ${PROJECT_HEX_FILE}
+  COMMENT "Flashing ${PROJECT_HEX_FILE} to ${AVRDUDE_MCU} on ${AVRDUDE_PORT}"
+  VERBATIM
+)
+
+
+message(STATUS "AVR Flash target added (Programmer: ${AVRDUDE_PROGRAMMER}, Port: ${AVRDUDE_PORT}, MCU: ${MCU})")
